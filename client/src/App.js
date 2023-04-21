1b536ebd949513c17426ec1cce54abdacc2799b7
lilyLILYLL <lydatily@gmail.com>
2023-04-21 20:25:30 +1000

    add mobile not supported page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
 import { Layout } from "../src/components/layout/Layout";
+import { MobileNotSupportedPage } from "../src/pages/MobileNotSupportedPage";
+import React, { useState, useEffect } from "react";
 function App() {
-    return <Layout />;
+    const [width, setWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        const updateWidth = () => {
+            setWidth(window.innerWidth);
+        };
+        const unsubcribe = window.addEventListener("resize", updateWidth);
+        return () => window.removeEventListener("resize", updateWidth);
+    }, []);
+
+    return width < 550 ? <MobileNotSupportedPage /> : <Layout />;
 }
 
 export default App;
---
+++ client/src/pages/MobileNotSupportedPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "../scss/MobileNotSupportedPage.scss";
+export const MobileNotSupportedPage = () => {
+    return <div className="not-supported">Mobile Device Not Supported! </div>;
+};
